5afd316fe9a266964bac4a5873cc68c7918b7578
Paul <paulrousset@hotmail.fr>
2022-11-07 16:54:29 +0100

    finalize main code

--- src/house_sales/predict.py
+++ src/house_sales/predict.py
@@ -4,11 +4,11 @@ import numpy as np
 import pandas as pd
 import importlib.metadata
 
+from src.house_sales import __version__ as _version
+
 from src.house_sales.config.core import config
 from src.house_sales.processing.data_manager import load_pipeline
 from src.house_sales.processing.validation import validate_inputs
-
-_version = importlib.metadata.version('house_sales')
 
 pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
 _price_pipe = load_pipeline(file_name=pipeline_file_name)
--- src/house_sales/processing/data_manager.py
+++ src/house_sales/processing/data_manager.py
@@ -32,3 +32,24 @@ def save_pipeline(*, pipeline_to_persist: Pipeline) -> None:
 
     remove_old_pipelines(files_to_keep=[save_file_name])
     joblib.dump(pipeline_to_persist, save_path)
+
+
+def load_pipeline(*, file_name: str) -> Pipeline:
+    """Load a persisted pipeline."""
+
+    file_path = TRAINED_MODEL_DIR / file_name
+    trained_model = joblib.load(filename=file_path)
+    return trained_model
+
+
+def remove_old_pipelines(*, files_to_keep: t.List[str]) -> None:
+    """
+    Remove old model pipelines.
+    This is to ensure there is a simple one-to-one
+    mapping between the package version and the model
+    version to be imported and used by other applications.
+    """
+    do_not_delete = files_to_keep + ["__init__.py"]
+    for model_file in TRAINED_MODEL_DIR.iterdir():
+        if model_file.name not in do_not_delete:
+            model_file.unlink()
