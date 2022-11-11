0d103b0f7f5db73f7da0df5856712020862a42b1
Paul <paulrousset@hotmail.fr>
2022-11-11 15:51:31 +0100

    fix typeguard test issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,23 @@
 """Test fixtures."""
 import pytest
+from typeguard import typechecked
+from typeguard.importhook import install_import_hook
 
 from house_sales.config.core import config
 from house_sales.processing.data_manager import load_dataset
+
+
+def pytest_configure():
+    """Install import hook."""
+    install_import_hook("house_sales")
+
+
+def pytest_runtest_call(item):
+    """Decorate every test function with typeguard's typechecked() decorator."""
+    test_func = getattr(item, "obj", None)
+    if test_func is not None:
+        # setattr(item, "obj", typechecked(test_func))
+        item.obj = typechecked(test_func)
 
 
 @pytest.fixture()
